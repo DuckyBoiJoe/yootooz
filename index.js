937d6ebec19c25c6d480caf09bf7560d0c6854ee
DuckyBoiJoe <65960470+DuckyBoiJoe@users.noreply.github.com>
2020-06-03 10:40:18 -0600

    Update index.js

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ bot.on('message', message => {
             const filter = m => m.author.id == message.author.id
             const collector = msg.channel.createMessageCollector(filter, { time: 180000, max: 1 })
             collector.on('collect', m => {
+                collector.stop()
                 if (m.content.toLowerCase == 'cancel') return msg.channel.send('Cancellled Prompt')
                 const channel = message.guild.channels.cache.find(c => c.name == 'suggestions' && c.type == 'text')
                 channel.send(`Username: ${message.author.username} \nSuggestion: ${m.content}`)
@@ -40,5 +41,20 @@ bot.on('message', message => {
         msg.react('👍')
         msg.author.send('Your suggestion has been approved! It will be in a later video.')
     }
+
+    if(message.content.startsWith(`${prefix}kick`)) {
+        if(!message.member.permissions.has('KICK_MEMBERS')) return message.channel.send('You don\'t have permissions to use that command!')
+        const user = message.mentions.members.first()
+        if(!user) return message.channel.send('You need to mention somebody!')
+        const member = message.guild.member(user)
+        if(!member) return message.channel.send('I couldn\'t find that member.')
+        const reason = args.splice(2).join(' ')
+        member.send(`You were kicked from Keerat_Law's Server. \nReason: ${reason}`)
+        member.kick(reason)
+        message.channel.send('**Successfully Kicked ' + member.user.username + '**')
+        
+    }
+
+    
 })
 bot.login(process.env.BOT_TOKEN)
