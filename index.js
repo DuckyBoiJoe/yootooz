e5750b25cc344302b60a0491f55eaa8a3bba123d
DuckyBoiJoe <65960470+DuckyBoiJoe@users.noreply.github.com>
2020-06-03 10:03:48 -0600

    Update index.js

--- index.js
+++ index.js
@@ -10,11 +10,23 @@ bot.on('guildMemberAdd', member => {
 bot.on('message', message => {
     let args = message.content.substring(prefix.length).split(/ +/)
 
-    if(message.content.startsWith(`${prefix}poll`)) {
+    if(message.content == `${prefix}poll`) {
         const channel = message.guild.channels.cache.find(c => c.name == 'polls' && c.type == 'text')
         channel.send(`@everyone Poll: ${args.splice(1).join(' ')}`).then(msg => {
             msg.react('👍')
             msg.react('👎')
+        })
+    }
+
+    if(message.content.startsWith(`${prefix}suggest`)) {
+        message.author.send('What would you like your suggestion to be?').then(msg => {
+            const filter = m => m.author.id == message.author.id
+            const collector = msg.channel.createMessageCollector(filter, { time: 180000, max: 1 })
+            collector.on('collect', m => {
+                if (m.content.toLowerCase == 'cancel') return msg.channel.send('Cancellled Prompt')
+                const channel = message.guild.channels.cache.find(c => c.name == 'suggestions' && c.type == 'text')
+                channel.send(`Username: ${message.author.username} \nSuggestion: ${m.content}`)
+            })
         })
     }
 })
